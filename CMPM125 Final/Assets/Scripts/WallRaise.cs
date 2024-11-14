ee738d1939c86002ecedb34c711e712b18a17b5e
Ashley Knapp <ajknapp@ucsc.edu>
2024-11-14 05:56:57 -0800

    Button Scripts and example prefabs

---
+++ CMPM125 Final/Assets/Scripts/WallRaise.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class WallRaise : MonoBehaviour
+{
+    // Alternate code to move this object to a new x,y,z location specified in the inspector
+    // public Vector3 newPosition;
+    // public void MoveTo(){
+    //     transform.position = newPosition;
+    // }
+
+    // Move this object to another object (likely an empty object)
+    public GameObject targetPos;
+    private Vector3 originalPosition;
+
+    void Start(){
+        originalPosition = transform.position;
+    }
+
+    public void MoveToObject(){
+        Debug.Log("Moving object to " + targetPos.transform.position);
+        transform.position = targetPos.transform.position;
+    }
+
+    public void RevertPosition(){
+        transform.position = originalPosition;
+    }
+}
