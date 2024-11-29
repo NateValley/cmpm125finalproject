7f90f4f984013f193fca0e624fff90cfc5aece4d
NateValley <46508762+NateValley@users.noreply.github.com>
2024-11-28 17:15:17 -0800

    Level 1 Puzzles Complete and Catapult Refinement

--- CMPM125 Final/Assets/Scripts/LaunchEgg.cs
+++ CMPM125 Final/Assets/Scripts/LaunchEgg.cs
@@ -1,25 +1,3 @@
-<<<<<<< Updated upstream:CMPM125 Final/Assets/LaunchEgg.cs
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-//script can be added to any object but Bullet1 should be assigned as egg
-public class Launch : MonoBehaviour
-{
-    public Rigidbody bullet1;//Egg should be assigned here
-	public int force;//How much force is used on launch
-	private void OnTriggerStay(Collider other)
-	{	
-		if (other.tag == "launch")//Make tag called "launch" and add it to "Trigger" gameobject in catapult prefab
-		{
-			Rigidbody bullet = bullet1;
-			bullet.gameObject.SetActive(true);
-			bullet.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * force, ForceMode.Impulse);
-		}
-	}
-
-}
- 
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -29,14 +7,21 @@ public class Launch : MonoBehaviour
 	public float force;//How much force is used on launch
 	private void OnCollisionEnter(Collision other)
 	{	
-		if (other.gameObject.tag == "Egg")//Make tag called "launch" and add it to "Trigger" gameobject in catapult prefab
+		if (other.gameObject.tag == "Egg")
 		{
 			Rigidbody eggRB = other.gameObject.GetComponent<Rigidbody>();
-			eggRB.AddForce((Vector3.up * 2.0f) * force, ForceMode.Impulse);
-			eggRB.AddForce(transform.forward * force, ForceMode.Impulse);
+
+			Vector3 newPosition = new Vector3(transform.position.x, eggRB.transform.position.y + 5, eggRB.transform.position.z + 5);
+
+			if (eggRB.transform.position != newPosition)
+			{
+				eggRB.velocity = Vector3.zero;
+				eggRB.transform.position = newPosition;
+			}
+
+			Vector3 launchForce = (Vector3.up * 2.0f + transform.forward) * force;
+			eggRB.AddForce(launchForce, ForceMode.Impulse);
 		}
 	}
 
 }
- 
->>>>>>> Stashed changes:CMPM125 Final/Assets/Scripts/LaunchEgg.cs
