8ebaacf65e3a5516cfb18e199e948bc28e4ab3e8
Kevin Gallegos <kevingallegos2003@gmail.com>
2024-12-05 03:32:55 -0800

    Placeholder Canvas up to lvl 1 wins screen works

--- CMPM125 Final/Assets/Scripts/TransformEGG.cs
+++ CMPM125 Final/Assets/Scripts/TransformEGG.cs
@@ -4,22 +4,25 @@ using UnityEngine.UI;
 using UnityEngine;
 
 public class TransformEGG : MonoBehaviour
-{
-    [SerializeField] Slider EggTracker;
+{   
+    public static float eggsZ = 0;
+
     [SerializeField] GameObject Egg;
+    Slider EggTracker;
     Vector3 ballpos;
     Transform ball;
     void Start()
-    {
-        ball = Egg.GetComponent<Transform>();
+    {   
+        EggTracker = GameObject.FindGameObjectWithTag("slider").GetComponent<Slider>();
         EggTracker.value = 0;
     }
     void Update()
     {   
-        //Transform ball = Egg.GetComponent<Transform>();
+        ball = Egg.GetComponent<Transform>();
         ballpos = ball.transform.position;
-         if(ballpos.z > -2.77){
+        if(ballpos.z > -2.77){
             //Debug.Log(ballpos.z + 2.76);
+            eggsZ = ballpos.z;
             EggTracker.value = (float)(ballpos.z + 2.76);
         }
     }
